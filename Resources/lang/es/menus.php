01d40fa22d8efd806fd601d1bb62f9c6ab882043
Angel Kurten <angelkurten@hotmail.com>
2015-11-06 23:16:16 -0500

    add sidebar lang

--- Resources/lang/es/menus.php
+++ Resources/lang/es/menus.php
@@ -1,12 +1,27 @@
 <?php
 
 return [
-    'principal' =>[
+    'principal'         =>  [
         'home'          => 'Inicio',
-        'about'      => 'Nosotros',
+        'about'         => 'Nosotros',
         'team'          => 'Equipo',
         'services'      => 'Servicios',
         'platform'      => 'Plataforma',
         'contact'       => 'Contacto'
     ],
+
+    'sidebar'           => [
+        'profile'       => 'Perfil',
+        'inbox'         => 'Mensajes',
+        'exit'          => 'Salir',
+        'courses'       => 'Cursos',
+        'social'        => 'Social',
+        'submenu'       => [
+            'ranking'   => [
+                'title' => 'Ranking',
+                'help'  => 'Solo quienes aprueben un examen o quiz ganaran puntos',
+                'global'=> 'Global'
+            ]
+        ]
+    ]
 ];
