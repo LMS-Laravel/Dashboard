66a101b996cfec473da53c7ae2b1654fa10f4715
Angel Kurten <angelkurten@hotmail.com>
2016-05-16 10:19:39 -0500

    Add English Lang

---
+++ Resources/lang/en/menus.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'principal'         =>  [
+        'home'          => 'Home',
+        'about'         => 'About',
+        'team'          => 'Team',
+        'services'      => 'Services',
+        'platform'      => 'Platform',
+        'contact'       => 'Contact'
+    ],
+
+    'sidebar'           => [
+        'profile'       => 'Profile',
+        'inbox'         => 'Messages',
+        'exit'          => 'Logout',
+        'courses'       => 'Courses',
+        'social'        => 'Feed',
+        'submenu'       => [
+            'ranking'   => [
+                'title' => 'Ranking',
+                'help'  => 'Only those who pass an exam or quiz earn points',
+                'global'=> 'Global'
+            ]
+        ]
+    ]
+];
--- Resources/lang/en/ui.php
+++ Resources/lang/en/ui.php
@@ -9,4 +9,45 @@ return [
         'num_permissions'               =>  'Cantidad de Permisos',
     ],
 
+    'frontend'                          => [
+        'welcome'                       => 'Welcome to :name'
+    ],
+
+    'buttons'                           => [
+        'about-me'                      => 'About Us',
+        'registration'                  => 'Sign up'
+    ],
+
+    'sections'                          => [
+        'about-me'                      => [
+            'name'                      => 'Services',
+            'description'               => 'LMS-Laravel is an educational management platform designed to be simple and easy',
+            'box-one'                   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec sapien at ante ullamcorper luctus eget at nunc. Duis nec massa vitae neque aliquet pharetra in quis purus. Pellentesque lacinia iaculis justo id bibendum. Maecenas euismod nunc eget metus consequat fermentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent at pharetra velit, vulputate consectetur mauris. Sed vel gravida risus, eget tincidunt tellus.',
+            'box-two'                   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec sapien at ante ullamcorper luctus eget at nunc. Duis nec massa vitae neque aliquet pharetra in quis purus. Pellentesque lacinia iaculis justo id bibendum. Maecenas euismod nunc eget metus consequat fermentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent at pharetra velit, vulputate consectetur mauris. Sed vel gravida risus, eget tincidunt tellus.',
+        ],
+        'team'                          => [
+            'name'                      => 'Team',
+            'description'               => ':name It is possible thanks to the hard work of these professionals.'
+        ],
+
+        'services'                      => [
+            'name'                      => 'Services',
+            'description'               => ':name we have thought of everything, and that is why we offer you the following solutions',
+            'items'                     => [
+                'one'                   => [
+                    'name'              => 'Training',
+                    'description'       => 'You find the best free and paid courses that can be found throughout the web, taught by leading professionals in the community'
+                ],
+                'two'                   => [
+                    'name'              => 'Q&A',
+                    'description'       => 'On our platform you will find a set of questions and answers, where you can use to expand your concerns.'
+                ],
+                'three'                 => [
+                    'name'              => 'Certifications',
+                    'description'       => 'Become a certified professional with all our system of official certification.'
+                ]
+            ]
+        ],
+        'contact'                       => 'Contact'
+    ]
 ];
